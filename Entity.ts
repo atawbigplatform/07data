fc186ddad9d1245a4e83b0359df17fdf92711278
zyking <lusess@qq.com>
2017-12-25 14:49:59 +0800

    初始化项目

---
+++ Entity.ts
@@ -0,0 +1,25 @@
+﻿
+
+export module entity {
+
+    export class SelectorItem {
+        public IsSelect: boolean;
+        public Key: string;
+        public Text: string;
+    }
+
+    export class SelectorModel {
+        public List: Array<any>;
+        public Total: number;
+        public Index: number;
+        public Size: number;
+    }
+
+    //票号和金额
+    export class NumMoney {
+        public Num: string;//规定是16位的
+        public Money: string;
+        public isCheck: boolean = true;
+    }
+
+} 
